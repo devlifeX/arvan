3251499c30b532c72c1dd81cafdbdd111c3ae2c4
Darius Vesal <dariush.vesal@gmail.com>
2019-10-24 03:04:54 +0330

    Add domain controller

---
+++ app/Http/Controllers/DomainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DomainController extends Controller
+{
+    public function create()
+    {
+
+        return ['test' => 32134];
+    }
+}
