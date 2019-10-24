b6f1482ac43de6b3d79864331cb45fb3065de2d3
Darius Vesal <dariush.vesal@gmail.com>
2019-10-24 09:40:13 +0330

    Add arvancloud terms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-10">
             <h2>All Doamins</h2>
             <show-domains></show-domains>
         </div>
-         <div class="col-md-4">
+         <div class="col-md-2">
              <a href="{{url('add')}}">Add new Domain</a>
         </div>
     </div>
