1c843feb8612e8ae694e37fbc4e991065c594e7e
Darius Vesal <dariush.vesal@gmail.com>
2019-11-09 20:39:42 +0330

    Improve url sanitize

--- app/MyUtil/MyHelper.php
+++ app/MyUtil/MyHelper.php
@@ -5,16 +5,26 @@ namespace App\MyUtil;
 class MyHelper
 {
 
-    static public function urlSanitize($url)
+    static public function getHost($url)
     {
-        $newUrl = parse_url(strtolower($url));
-        $getHost = function ($url) {
+        try {
             $parts = explode('.', $url);
             if (count($parts) === 2) return $url;
             $rev = array_reverse($parts);
             return $rev[1] . '.' . $rev[0];
-        };
-        $output =  $newUrl['scheme'] . "://www." .  $getHost($newUrl['host']);
+        } catch (\Throwable $th) {
+            return null;
+        }
+    }
+
+    static public function urlSanitize($url)
+    {
+        $newUrl = parse_url(strtolower($url));
+        $host  = self::getHost($newUrl['host']);
+        if (is_null($host) || empty($host)) {
+            throw new \Exception("Bad URL");
+        }
+        $output =  $newUrl['scheme'] . "://www." . $host;
         return $output;
     }
 }
